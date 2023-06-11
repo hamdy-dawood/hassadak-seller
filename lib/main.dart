0c613eafdb9eb48a828e14d118e3384de29039b3
hamdy <hamdykhalid12345@gmail.com>
2023-06-12 01:35:23 +0200

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'pages/upload_user_photo/cubit.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
-  CacheHelper.clear();
+  // CacheHelper.clear();
   runApp(const MyApp());
 }
 
